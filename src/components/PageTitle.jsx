95ef0ef5c59c9138d5418b989d2d0e7ba11fadc0
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-05-25 11:38:09 +0530

    Page title created

---
+++ src/components/PageTitle.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import style from '../styles/modules/title.module.scss';
+
+export default function PageTitle({children,...rest}) {
+  return <p className={style.title} {...rest}>{children}</p>
+}
