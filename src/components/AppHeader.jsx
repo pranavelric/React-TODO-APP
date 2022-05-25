725bb429912c4246331458d18546171bf93de2b4
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-05-25 21:18:54 +0530

    Todo Modal created

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button, { SelectButton } from './Button';
 import styles from '../styles/modules/app.module.scss';
 import TodoModal from './TodoModal';
 
 export default function AppHeader() {
+  const [modelOpen,setModalOpen] = useState(false);
   return (
       <div className={styles.app_header}>
-    <Button variant='primary'>AddTask</Button>
+    <Button variant='primary' onClick={()=>setModalOpen(true)}>AddTask</Button>
     <SelectButton>
         <option value="all">All</option>
         <option value="Complete">Complete</option>
         <option value="Incomplete">Incomplete</option>
     </SelectButton>
-    <TodoModal/>
+    <TodoModal modelOpen={modelOpen} setModalOpen={setModalOpen}/>
   </div>
   )
 }
--- src/components/TodoModal.jsx
+++ src/components/TodoModal.jsx
@@ -1,35 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/modules/modal.module.scss';
 import {MdOutlineClose} from 'react-icons/md';
+import {useDispatch} from 'react-redux';
 import Button from './Button';
+import { addTodo } from '../slices/todoSlice';
+import {v4 as uuid} from 'uuid';
+import toast from 'react-hot-toast';
 
-export default function TodoModal() {
-  return (
+export default function TodoModal(props) {
+    
+    const dispatch = useDispatch();
+
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        if(title && status){
+            dispatch(addTodo({
+                id: uuid(),
+                title:title,
+                status:status,
+                time: new Date().toLocaleString(),
+            }));
+            toast.success('Task Added Successfully');
+            props.setModalOpen(false)
+        }
+        else{
+            toast.error("Title shouldn't be empty")
+        }
+    }
+    const [title, setTitle] = useState('');
+    const [status,setStatus] = useState('incomplete');
+    return (
+    props.modelOpen && (
     <div className={styles.wrapper}>
         <div className={styles.container}>
-            <div className={styles.closeButton}>
+            <div className={styles.closeButton} onClick={()=>props.setModalOpen(false)}>
                 <MdOutlineClose></MdOutlineClose>
             </div>
-            <form action="" className={styles.form}>
+            <form onSubmit={(e)=>handleSubmit(e)} className={styles.form}>
                 <h1 className={styles.formTitle}>Add Task</h1>
                 <label htmlFor='title'>
                 Title
-                <input type='text' id='title'/>    
+                <input type='text' id='title' value={title}
+                onChange={(e)=>setTitle(e.target.value)}
+                />    
                 </label>    
 
                 <label htmlFor='status'>
                 Status
-                <select type='status' id='status'>
+                <select type='status' id='status' value={status} onChange={(e)=>setStatus(e.target.value)}>
                     <option value="incomplete">Incomplete</option>
                     <option value="complete">Complete</option>
                 </select>    
                 </label>    
                 <div className={styles.buttonContainer}>
                     <Button type="submit" variant='primary'>Add Task</Button>
-                    <Button type="button" variant='secondary'>Cancel</Button>
+                    <Button type="button" variant='secondary'  onClick={()=>props.setModalOpen(false)}>Cancel</Button>
                 </div>
             </form>
         </div>
     </div>
-  )
+    )
+  );
 }
